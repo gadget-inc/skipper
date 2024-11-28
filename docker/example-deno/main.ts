bf9685375272020cc7e7c6828ed6588524062bba
Scott Côté <scott.cote@hey.com>
2024-11-28 11:40:32 -0500

    WIP - Thu, Nov 28 11:40 AM

--- docker/example-deno/main.ts
+++ docker/example-deno/main.ts
@@ -2,69 +2,72 @@ import { pino } from "npm:pino";
 import pretty from "npm:pino-pretty";
 const log = pino(pretty());
 
+const assignPath = new URLPattern({ pathname: "/__fusion/assign" });
 const healthzPath = new URLPattern({ pathname: "/healthz" });
 
-const assignPath = new URLPattern({ pathname: "/__fusion/assign" });
-const assignFilePath = "/tmp/assignment.json";
-let assignmentHeaders = await Deno.readTextFile(assignFilePath).then((txt) => JSON.parse(txt)).catch(() => null);
+const assignedPath = "/tmp/assigned";
+let assigned = await Deno.stat(assignedPath).then(() => true).catch(() => false);
 
 const server = Deno.serve({ port: 8888 }, async (request) => {
-    if (request.method === "GET" && healthzPath.test(request.url)) {
-        return new Response();
+  if (request.method === "GET" && healthzPath.test(request.url)) {
+    return new Response();
+  }
+
+  const headers = Object.fromEntries(request.headers.entries());
+  log.info({ method: request.method, url: request.url, headers }, "incoming request");
+
+  if (request.method === "POST" && assignPath.test(request.url)) {
+    if (assigned) {
+      return new Response("already assigned", { status: 409 });
     }
 
-    log.info({
+    assigned = true;
+    await Deno.writeTextFile(assignedPath, "");
+
+    return new Response();
+  }
+
+  if (!assigned) {
+    return new Response("not assigned", { status: 503 });
+  }
+
+  if (request.headers.get("upgrade") !== "websocket") {
+    return new Response(
+      JSON.stringify({
         method: request.method,
         url: request.url,
         headers: Object.fromEntries(request.headers.entries()),
-    }, "incoming request");
-
-    if (request.method === "POST" && assignPath.test(request.url)) {
-        if (assignmentHeaders) {
-            return new Response("already assigned", { status: 409 });
-        }
-
-        assignmentHeaders = Object.fromEntries(request.headers.entries());
-        await Deno.writeTextFile(assignFilePath, JSON.stringify(assignmentHeaders, null, 2));
-
-        log.info({ assignmentHeaders }, "assigned");
-        return new Response();
-    }
-
-    if (!assignmentHeaders) {
-        return new Response("not assigned", { status: 503 });
-    }
-
-    if (request.headers.get("upgrade") === "websocket") {
-        const { socket, response } = Deno.upgradeWebSocket(request);
-
-        socket.addEventListener("open", () => {
-            log.info("websocket opened");
-        });
-
-        socket.addEventListener("message", (event) => {
-            log.info({ data: event.data }, "websocket message");
-            if (event.data === "ping") {
-                socket.send("pong");
-            }
-        });
-
-        return response;
-    }
-
-    return new Response(request.body, {
+        body: await request.text(),
+      }),
+      {
         headers: {
-            "content-type": request.headers.get("content-type")!,
+          "content-type": "application/json",
         },
-    });
+      },
+    );
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(request);
+
+  socket.addEventListener("open", () => {
+    log.info("websocket opened");
+  });
+
+  socket.addEventListener("message", (event) => {
+    log.info({ data: event.data }, "websocket message");
+    if (event.data === "ping") {
+      socket.send("pong");
+    }
+  });
+
+  return response;
 });
 
 Deno.addSignalListener("SIGTERM", async () => {
-    log.info("shutting down");
-    await server.shutdown();
-    log.info("shutdown");
+  log.info("shutting down");
+  await server.shutdown();
+  log.info("shutdown");
 });
 
 await server.finished;
-
 log.info("done");
---
+++ internal/controller/controller_test.go
@@ -0,0 +1,44 @@
+package controller_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/gadget-inc/fusion/internal/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestController(t *testing.T) {
+	fixture := test.NewFixture(t, "test-controller")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "http://localhost:31020/", bytes.NewBufferString("hello world!"))
+	assert.NoError(t, err, "failed to create request")
+
+	req.Header.Set("Content-Type", "text/plain")
+	fixture.Function.SetHeaders(req)
+
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err, "failed to send request")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "unexpected status code")
+
+	var response struct {
+		Method  string            `json:"method"`
+		URL     string            `json:"url"`
+		Headers map[string]string `json:"headers"`
+		Body    string            `json:"body"`
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&response)
+	assert.NoError(t, err, "failed to decode response")
+	assert.Equal(t, http.MethodPost, response.Method, "unexpected method")
+	assert.Equal(t, "http://localhost:31020/", response.URL, "unexpected url")
+	assert.Equal(t, "hello world!", response.Body, "unexpected body")
+	assert.Equal(t, "text/plain", response.Headers["content-type"], "unexpected content type")
+}
