fb6c6380042ec362556b26aae1122e63c1631e4f
Scott Côté <scott.cote@hey.com>
2025-01-31 15:05:30 -0500

    WIP - Fri, Jan 31 03:05 pm

--- internal/controller/scale_test.go
+++ internal/controller/scale_test.go
@@ -27,21 +27,6 @@ func init() {
 }
 
 func TestAssignPodToFunction(t *testing.T) {
-	ensureInstanceMatchesPod := func(instance *function.Instance, pod v1.Pod) {
-		must.Eq(t, instance.Function.Deployment, pod.Labels[key.Deployment.Label])
-		must.Eq(t, instance.Function.Tenant, pod.Labels[key.Tenant.Label])
-
-		fnJSON, err := json.Marshal(instance.Function)
-		must.NoError(t, err)
-		must.Eq(t, string(fnJSON), pod.Annotations[key.Function.Label])
-
-		must.Eq(t, instance.Name, pod.Name)
-		must.Eq(t, instance.Addr, pod.Status.PodIP+":"+strconv.Itoa(int(pod.Spec.Containers[0].Ports[0].ContainerPort)))
-		must.Eq(t, instance.Version, pod.Annotations[key.ReplicaSet.Label])
-		must.Eq(t, instance.AssignedAt.Format(time.RFC3339), pod.Annotations[key.AssignedAt.Label])
-		must.Eq(t, instance.ReadyAt.Format(time.RFC3339), pod.Annotations[key.ReadyAt.Label])
-	}
-
 	testCases := []struct {
 		name  string
 		err   error
@@ -58,7 +43,7 @@ func TestAssignPodToFunction(t *testing.T) {
 				pods, err := clientset.CoreV1().Pods(instance.Namespace).List(context.Background(), metav1.ListOptions{})
 				must.NoError(t, err)
 				must.Len(t, 1, pods.Items)
-				ensureInstanceMatchesPod(instance, pods.Items[0])
+				ensureInstanceMatchesPod(t, instance, pods.Items[0])
 			},
 		},
 		{
@@ -83,7 +68,7 @@ func TestAssignPodToFunction(t *testing.T) {
 				pods, err := clientset.CoreV1().Pods(instance.Namespace).List(context.Background(), metav1.ListOptions{})
 				must.NoError(t, err)
 				must.Len(t, 1, pods.Items)
-				ensureInstanceMatchesPod(instance, pods.Items[0])
+				ensureInstanceMatchesPod(t, instance, pods.Items[0])
 			},
 		},
 	}
@@ -122,64 +107,73 @@ func TestAssignPodToFunction(t *testing.T) {
 
 func TestScaleFunction(t *testing.T) {
 	testCases := []struct {
-		name              string
-		desiredInstances  int
-		expectedInstances int
-		err               error
-		setup             func(*testing.T, *fake.Clientset, function.Function)
-		// check func(*testing.T, *fake.Clientset, *function.Instance)
+		name             string
+		desiredInstances int
+		err              error
+		setup            func(*testing.T, *fake.Clientset, function.Function)
+		check            func(*testing.T, *fake.Clientset, []*function.Instance)
 	}{
 		{
-			name:              "smoke",
-			desiredInstances:  1,
-			expectedInstances: 1,
-			err:               nil,
+			name:             "smoke",
+			desiredInstances: 1,
+			err:              nil,
 			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
 				err := clientset.Tracker().Add(fixture.NewAvailablePod(t, fn, nil))
 				must.NoError(t, err)
 			},
+			check: func(t *testing.T, clientset *fake.Clientset, instances []*function.Instance) {
+				must.Len(t, 1, instances)
+			},
 		},
 		{
-			name:              "extra available pods",
-			desiredInstances:  1,
-			expectedInstances: 1,
-			err:               nil,
+			name:             "extra available pods",
+			desiredInstances: 1,
+			err:              nil,
 			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
 				for i := 0; i < 5; i++ {
 					err := clientset.Tracker().Add(fixture.NewAvailablePod(t, fn, nil))
 					must.NoError(t, err)
 				}
 			},
-		},
-		{
-			name:              "no available pods",
-			desiredInstances:  1,
-			expectedInstances: 0,
-			err:               context.DeadlineExceeded,
-			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
-				// no pods
+			check: func(t *testing.T, clientset *fake.Clientset, instances []*function.Instance) {
+				must.Len(t, 1, instances)
 			},
 		},
 		{
-			name:              "already has desired instances",
-			desiredInstances:  1,
-			expectedInstances: 1,
-			err:               nil,
+			name:             "no available pods",
+			desiredInstances: 1,
+			err:              context.DeadlineExceeded,
+			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
+				// no pods
+			},
+			check: func(t *testing.T, clientset *fake.Clientset, instances []*function.Instance) {
+				must.Len(t, 0, instances)
+			},
+		},
+		{
+			name:             "already has desired instances",
+			desiredInstances: 1,
+			err:              nil,
 			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
 				err := clientset.Tracker().Add(fixture.NewAssignedPod(t, fn, nil))
 				must.NoError(t, err)
 			},
+			check: func(t *testing.T, clientset *fake.Clientset, instances []*function.Instance) {
+				must.Len(t, 1, instances)
+			},
 		},
 		{
-			name:              "scale down",
-			desiredInstances:  1,
-			expectedInstances: 1,
-			err:               nil,
+			name:             "scale down",
+			desiredInstances: 1,
+			err:              nil,
 			setup: func(t *testing.T, clientset *fake.Clientset, fn function.Function) {
 				for i := 0; i < 5; i++ {
 					err := clientset.Tracker().Add(fixture.NewAssignedPod(t, fn, nil))
 					must.NoError(t, err)
 				}
+			},
+			check: func(t *testing.T, clientset *fake.Clientset, instances []*function.Instance) {
+				must.Len(t, 1, instances)
 			},
 		},
 	}
@@ -206,11 +200,26 @@ func TestScaleFunction(t *testing.T) {
 			instances, err := c.scaleFunction(ctx, fn, tc.desiredInstances)
 			if tc.err != nil {
 				must.ErrorIs(t, err, tc.err)
-				return
+			} else {
+				must.NoError(t, err)
 			}
 
-			must.NoError(t, err)
-			must.Len(t, tc.expectedInstances, instances)
+			tc.check(t, clientset, instances)
 		})
 	}
+}
+
+func ensureInstanceMatchesPod(t *testing.T, instance *function.Instance, pod v1.Pod) {
+	must.Eq(t, instance.Function.Deployment, pod.Labels[key.Deployment.Label])
+	must.Eq(t, instance.Function.Tenant, pod.Labels[key.Tenant.Label])
+
+	fnJSON, err := json.Marshal(instance.Function)
+	must.NoError(t, err)
+	must.Eq(t, string(fnJSON), pod.Annotations[key.Function.Label])
+
+	must.Eq(t, instance.Name, pod.Name)
+	must.Eq(t, instance.Addr, pod.Status.PodIP+":"+strconv.Itoa(int(pod.Spec.Containers[0].Ports[0].ContainerPort)))
+	must.Eq(t, instance.Version, pod.Annotations[key.ReplicaSet.Label])
+	must.Eq(t, instance.AssignedAt.Format(time.RFC3339), pod.Annotations[key.AssignedAt.Label])
+	must.Eq(t, instance.ReadyAt.Format(time.RFC3339), pod.Annotations[key.ReadyAt.Label])
 }
