c4abee1820666d86041d97522b1e7e6874c8a0ab
Scott Côté <scott.cote@hey.com>
2025-02-04 23:39:42 -0500

    WIP - Tue, Feb 04 11:39 PM

--- scripts/build-fixtures.ts
+++ scripts/build-fixtures.ts
@@ -7,7 +7,7 @@ const sha = await gitSha();
 for (const fixture of await glob(abs("docker/fixtures/*"), { onlyDirectories: true })) {
   $.cwd = fixture;
   const name = path.basename(fixture);
-  await $`docker build . --tag=fusion-fixture-${name}:${sha} --platform=linux/amd64,linux/arm64 --load`;
+  await $`docker buildx build . --tag=fusion-fixture-${name}:${sha} --platform=linux/amd64,linux/arm64 --load`;
   if (Deno.env.has("CI")) {
     await $`kind load docker-image fusion-fixture-${name}:${sha}`;
   }
--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,7 @@ import { abs, gitSha } from "./_utils.ts";
 $.cwd = abs();
 
 const sha = await gitSha();
-await $`docker build . --tag=fusion:${sha} --platform=linux/amd64,linux/arm64 --load`;
+await $`docker buildx build . --tag=fusion:${sha} --platform=linux/amd64,linux/arm64 --load`;
 if (Deno.env.has("CI")) {
   await $`kind load docker-image fusion:${sha}`;
 }
