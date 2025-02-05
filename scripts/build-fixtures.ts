0e4b59f2cd8b8249f57be3ff4e27dab770193ff4
Scott Côté <scott.cote@hey.com>
2025-02-05 18:41:50 -0500

    WIP - Wed, Feb 05 06:41 PM

--- scripts/build-fixtures.ts
+++ scripts/build-fixtures.ts
@@ -4,10 +4,15 @@ import { abs, gitSha, isCI } from "./_utils.ts";
 
 const sha = await gitSha();
 
+const platforms = ["linux/amd64"];
+if (!isCI) {
+  platforms.push("linux/arm64");
+}
+
 for (const fixture of await glob(abs("fixtures/*"), { onlyDirectories: true })) {
   $.cwd = fixture;
   const name = path.basename(fixture);
-  await $`docker buildx build . --tag=fusion-fixture-${name}:${sha} --platform=linux/amd64,linux/arm64 --load`;
+  await $`docker buildx build . --tag=fusion-fixture-${name}:${sha} --platform=${platforms.join(",")} --load`;
   if (isCI) {
     await $`kind load docker-image fusion-fixture-${name}:${sha}`;
   }
--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,12 @@ import { abs, gitSha, isCI } from "./_utils.ts";
 const sha = await gitSha();
 $.cwd = abs();
 
-await $`docker buildx build . --tag=fusion:${sha} --platform=linux/amd64,linux/arm64 --load`;
+const platforms = ["linux/amd64"];
+if (!isCI) {
+  platforms.push("linux/arm64");
+}
+
+await $`docker buildx build . --tag=fusion:${sha} --platform=${platforms.join(",")} --load`;
 if (isCI) {
   await $`kind load docker-image fusion:${sha}`;
 }
