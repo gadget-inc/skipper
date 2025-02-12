312480a2c36a56e87c290dde3e18b8513ebe28e6
Scott Côté <scott.cote@hey.com>
2025-02-11 22:46:43 -0500

    WIP - Tue, Feb 11 10:46 PM

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,17 +3,33 @@ import { $ } from "npm:zx";
 import { abs, currentDockerPlatform, defaultImageTag, isCI } from "./_utils.ts";
 import { parseArgs } from "jsr:@std/cli/parse-args";
 
+$.cwd = abs();
+$.verbose = true;
+
 const flags = parseArgs(Deno.args, {
   string: ["tag", "platform", "cache-from-to"],
-  boolean: ["kind"],
+  boolean: ["load", "push", "kind"],
   negatable: ["kind"],
   default: {
+    image: "fusion",
     tag: await defaultImageTag(),
     platform: await currentDockerPlatform(),
     kind: isCI,
     "cache-from-to": isCI ? "gha" : "",
+    load: true,
+    push: false,
   },
 });
+
+let load = "";
+if (flags.load) {
+  load = `--load`;
+}
+
+let push = "";
+if (flags.push) {
+  push = `--push`;
+}
 
 let cacheFromTo = "";
 switch (flags["cache-from-to"]) {
@@ -21,15 +37,12 @@ switch (flags["cache-from-to"]) {
     cacheFromTo = "--cache-from=type=gha --cache-to=type=gha,mode=max";
     break;
   case "registry":
-    cacheFromTo =
-      "--cache-from=type=registry,ref=us-central1-docker.pkg.dev/gadget-core-production/core-production/fusion:buildcache --cache-to=type=registry,ref=us-central1-docker.pkg.dev/gadget-core-production/core-production/fusion:buildcache,mode=max";
+    cacheFromTo = `--cache-from=type=registry,ref=${flags.image}:buildcache --cache-to=type=registry,ref=${flags.image}:buildcache,mode=max`;
     break;
 }
 
-$.cwd = abs();
-
-await $`docker buildx build . --load --tag=fusion:${flags.tag} --platform=${flags.platform} ${cacheFromTo}`;
+await $`docker buildx build . --tag=${flags.image}:${flags.tag} --platform=${flags.platform} ${cacheFromTo} ${load} ${push}`;
 
 if (flags.kind) {
-  await $`kind load docker-image fusion:${flags.tag}`;
+  await $`kind load docker-image ${flags.image}:${flags.tag}`;
 }
