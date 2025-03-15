a259fddf7a66d18614ea57788f0d486cbf48f59c
Scott Côté <scott.cote@hey.com>
2025-03-15 13:13:42 -0400

    WIP - Sat, Mar 15 01:13 PM

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -107,23 +107,23 @@ func (ctrl *Controller) startInformers(ctx context.Context) error {
 			pod := obj.(*v1.Pod)
 			if pod.Status.Phase == v1.PodRunning && pod.Status.PodIP != "" {
 				ctrl.ring.Add(pod.Status.PodIP)
-				log.Debug(ctx, "added controller", key.Pod.Field(pod), key.ResponsibleIP.Field(pod.Status.PodIP), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
+				log.Debug(ctx, "added controller", key.Pod.Field(pod), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
 			}
 		},
 		UpdateFunc: func(_, newObj any) {
 			pod := newObj.(*v1.Pod)
 			if pod.Status.Phase == v1.PodRunning && pod.DeletionTimestamp == nil && pod.Status.PodIP != "" {
 				ctrl.ring.Add(pod.Status.PodIP)
-				log.Debug(ctx, "updated controller", key.Pod.Field(pod), key.ResponsibleIP.Field(pod.Status.PodIP), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
+				log.Debug(ctx, "updated controller", key.Pod.Field(pod), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
 			} else {
 				ctrl.ring.Remove(pod.Status.PodIP)
-				log.Debug(ctx, "removed updated controller", key.Pod.Field(pod), key.ResponsibleIP.Field(pod.Status.PodIP), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
+				log.Debug(ctx, "removed updated controller", key.Pod.Field(pod), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
 			}
 		},
 		DeleteFunc: func(obj any) {
 			pod := obj.(*v1.Pod)
 			ctrl.ring.Remove(pod.Status.PodIP)
-			log.Debug(ctx, "removed deleted controller", key.Pod.Field(pod), key.ResponsibleIP.Field(pod.Status.PodIP), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
+			log.Debug(ctx, "removed deleted controller", key.Pod.Field(pod), slog.String("ring", strings.Join(ctrl.ring.List(), ",")))
 		},
 	})
 	if err != nil {
